bdc3e9a92b9fbcbbcd0162085efd499977cc8f69
athifsaleem2 <85497875+athifsaleem2@users.noreply.github.com>
2026-05-01 02:08:44 +0530

    Change apiUrl to localhost for development

--- ERPENTERPRISE/src/environments/environment.development.ts
+++ ERPENTERPRISE/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://app-erp-api-backend-gac6a2bubgcycahz.centralindia-01.azurewebsites.net/api'
+  apiUrl: 'http://localhost:52888/api'
 };
