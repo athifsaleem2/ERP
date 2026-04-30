0f43cb7b63c186af8da0bcf4643c719fd1e860aa
athifsaleem2 <85497875+athifsaleem2@users.noreply.github.com>
2026-05-01 02:08:25 +0530

    Update API URL for production environment

--- ERPENTERPRISE/src/environments/environment.ts
+++ ERPENTERPRISE/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:52888/api'
+  apiUrl: 'https://app-erp-api-backend-gac6a2bubgcycahz.centralindia-01.azurewebsites.net/api'
 };
